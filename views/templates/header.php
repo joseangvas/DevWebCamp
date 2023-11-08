3711e24ac5fc65b46de20fcae1262d1cad2721a8
Joseangvas <developer.javp@gmail.com>
2023-11-08 18:36:58 -0400

    Codificar Header.

---
+++ views/templates/header.php
@@ -0,0 +1,22 @@
+<header class="header">
+  <div class="header__contenedor">
+    <nav class="header__navegacion">
+      <a href="/registro" class="header__enlace">Registro</a>
+      <a href="/login" class="header__enlace">Iniciar Sesión</a>
+    </nav>
+
+    <div class="header__contenido">
+      <a href="/">
+        <h1 class="header__logo">
+          &#60;DevWebCamp />
+        </h1>
+      </a>
+
+      <p class="header__texto">Diciembre 4-5-2023</p>
+      <p class="header__texto header__texto--modalidad">En Linea - Presencial</p>
+
+      <a href="/registro" class="header__boton">Comprar Pase</a>
+
+    </div>
+  </div>
+</header>
