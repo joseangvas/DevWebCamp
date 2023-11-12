70b7bf3f2d0654a8800ab563cc72479400f0f8dd
Joseangvas <developer.javp@gmail.com>
2023-11-11 20:38:05 -0400

    Crear estilos CSS al Footer.

---
+++ views/templates/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+  <div class="footer__grid">
+    <div class="footer__contenido">
+      <h3 class="footer__logo">
+        &#60;DevWebCamp />
+      </h3>
+
+      <p class="footer__texto">
+        DevWebCamp es una Conferencia para Desarrolladores de Todos los Niveles y se lleva a cabo de forma Presencial y en Línea.
+      </p>
+    </div>
+
+    <nav class="menu-redes"> 
+      <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.facebook.com/joseangvas">
+          <span class="menu-redes__ocultar">Facebook</span>
+      </a> 
+      <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://twitter.com/joseangvas_dev">
+          <span class="menu-redes__ocultar">Twitter</span>
+      </a> 
+      <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.youtube.com/codejavtech">
+          <span class="menu-redes__ocultar">YouTube</span>
+      </a> 
+      <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://www.instagram.com/joseangvas/?hl=es">
+          <span class="menu-redes__ocultar">Instagram</span>
+      </a> 
+      <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://tiktok.com/@joseangvas">
+          <span class="menu-redes__ocultar">Tiktok</span>
+      </a> 
+      <a class="menu-redes__enlace" rel="noopener noreferrer" target="_blank" href="https://github.com/joseangvas">
+          <span class="menu-redes__ocultar">Github</span>
+      </a>
+    </nav>
+  </div>
+
+  <p class="footer__copyright">
+    DevWebCamp
+    <span class="footer__copyright--regular">
+      © Todos los derechos reservados <?php echo date('Y'); ?> | Diseñado por: <a href="#" target="_blank">José Angel Vásquez</a>
+    </span>
+  </p>
+</footer>
