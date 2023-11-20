e0dc5b15728e6a97a574fa04a4c9f0cb69af911a
Joseangvas <developer.javp@gmail.com>
2023-11-20 06:28:58 -0400

    Crear Endpoint de Eventos.

--- controllers/EventosController.php
+++ controllers/EventosController.php
@@ -9,7 +9,18 @@ class EventosController {
   public static function index(Router $router) {
 
     $router->render('admin/eventos/index', [
-      'titulo' => 'Conferencias y Workshops'
+      'titulo' => 'Conferencias y Eventos'
+    ]);
+  }
+
+  public static function crear(Router $router) {
+    $alertas = [];
+
+
+
+    $router->render('admin/eventos/crear', [
+      'titulo' => 'Registrar Conferencia / Evento',
+      'alertas' => $alertas
     ]);
   }
 }
--- views/admin/eventos/crear.php
+++ views/admin/eventos/crear.php
@@ -1,7 +1,7 @@
 <h2 class="dashboard__heading"><?php echo $titulo; ?></h2>
 
 <div class="dashboard__contenedor-boton">
-  <a class="dashboard__boton" href="/admin/ponentes">
+  <a class="dashboard__boton" href="/admin/eventos">
     <i class="fa-solid fa-circle-arrow-left"></i>
     Volver
   </a>
@@ -12,9 +12,9 @@
     include_once __DIR__ . '/../../templates/alertas.php';
   ?>
 
-  <form method="POST" action="/admin/ponentes/crear" enctype="multipart/form-data" class="formulario">  // enctype solo si hay imagenes.
+  <form method="POST" action="/admin/eventos/crear" class="formulario">
     <?php include_once __DIR__ . '/formulario.php'; ?>
 
-    <input class="formulario__submit formulario__submit--registrar" type="submit" value="Registrar Ponente">
+    <input class="formulario__submit formulario__submit--registrar" type="submit" value="Registrar Evento">
   </form>
 </div>
