37b6fec99e7342e7d65b3d1fa7b15733516b9d88
Joseangvas <developer.javp@gmail.com>
2023-11-20 17:58:23 -0400

    Crear Modelo Evento y Método Crear.

--- controllers/EventosController.php
+++ controllers/EventosController.php
@@ -4,6 +4,7 @@ namespace Controllers;
 
 use Model\Categoria;
 use Model\Dia;
+use Model\Evento;
 use Model\Hora;
 use MVC\Router;
 
@@ -23,12 +24,29 @@ class EventosController {
     $dias = Dia::all('ASC');
     $horas = Hora::all('ASC');
 
+    $evento = new Evento;
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+      $evento->sincronizar($_POST);
+      $alertas = $evento->validar();
+
+      if(!$alertas) {
+        $resultado = $evento->guardar();
+
+        if($resultado) {
+          header('Location: /admin/eventos/crear');
+        }
+      }
+    }
+
     $router->render('admin/eventos/crear', [
       'titulo' => 'Registrar Conferencia / Evento',
       'alertas' => $alertas,
       'categorias' => $categorias,
       'dias' => $dias,
-      'horas' => $horas
+      'horas' => $horas,
+      'evento' => $evento
     ]);
   }
 }
--- views/admin/eventos/formulario.php
+++ views/admin/eventos/formulario.php
@@ -9,6 +9,7 @@
       id="nombre"
       name="nombre"
       placeholder="Nombre del Evento"
+      value="<?php echo $evento->nombre; ?>"
     />
   </div>
 
@@ -20,41 +21,40 @@
       name="descripcion"
       placeholder="Descripción del Evento"
       rows="8"
-    ></textarea>
+    ><?php echo $evento->descripcion; ?></textarea>
   </div>
 
   <div class="formulario__campo">
-    <label for="descripcion" class="formulario__label">Tipo de Evento:</label>
+    <label for="categoria" class="formulario__label">Tipo de Evento:</label>
     <select class="formulario__select" name="categoria_id" id="categoria">
       <option value="">-- Seleccione una opción --</option>
       <?php foreach($categorias as $categoria) { ?>
-        <option value="<?php echo $categoria->id; ?>"><?php echo $categoria->nombre; ?></option>
+        <option <?php echo ($evento->categoria_id === $categoria->id) ? 'selected' : ''; ?> value="<?php echo $categoria->id; ?>"><?php echo $categoria->nombre; ?></option>
       <?php } ?>
     </select>
+  </div>
 
-    <div class="formulario__campo">
-      <label for="dia" class="formulario__label">Seleccione el Día:</label>
+  <div class="formulario__campo">
+    <label for="dia" class="formulario__label">Seleccione el Día:</label>
 
-      <div class="formulario__radio">
-        <?php foreach($dias as $dia) { ?>
-          <div>
-            <label for="<?php echo strtolower($dia->nombre); ?>"><?php echo $dia->nombre; ?></label>
+    <div class="formulario__radio">
+      <?php foreach($dias as $dia) { ?>
+        <div>
+          <label for="<?php echo strtolower($dia->nombre); ?>"><?php echo $dia->nombre; ?></label>
 
-            <input
-              type="radio"
-              id="<?php echo strtolower($dia->nombre); ?>"
-              name="dia"
-              value="<?php echo $dia->id; ?>"
-            />
-          </div>
-        <?php } ?>
-      </div>
-
+          <input
+            type="radio"
+            id="<?php echo strtolower($dia->nombre); ?>"
+            name="dia"
+            value="<?php echo $dia->id; ?>"
+          />
+        </div>
+      <?php } ?>
     </div>
   </div>
 
   <div class="formulario__campo" id="horas">
-    <label for="" class="formulario__label">Seleccione la Hora:</label>
+    <label for="hora" class="formulario__label">Seleccione la Hora:</label>
 
     <ul class="horas">
       <?php foreach($horas as $hora) { ?>
@@ -86,6 +86,7 @@
       id="disponibles"
       name="disponibles"
       placeholder="Cupos Disponibles"
+      value="<?php echo $evento->disponibles; ?>"
     />
   </div>
 </fieldset>
