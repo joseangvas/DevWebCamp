e3c13f2da3cf3d289a63c0d08544ca20f56c5a87
Joseangvas <developer.javp@gmail.com>
2023-11-27 19:07:53 -0400

    Crear enlace del Header.

--- views/templates/admin-header.php
+++ views/templates/admin-header.php
@@ -8,7 +8,6 @@
 
     <nav class="dashboard__nav">
       <form method="POST" action="/logout" class="dashboard__form">
-
         <input type="submit" value="Cerrar Sesión" class="dashboard__submit--logout">
       </form>
     </nav>
