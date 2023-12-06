84e64ec805fb353dfa49c765962c92a2de77aa6e
Joseangvas <developer.javp@gmail.com>
2023-12-06 06:31:12 -0400

    Adaptar Grafica de Regalos en Admin.

--- views/admin-layout.php
+++ views/admin-layout.php
@@ -12,25 +12,27 @@
     </head>
 
     <body class="dashboard">
-            <?php 
-                include_once __DIR__ .'/templates/admin-header.php';
-            ?>
-            
-            <div class="dashboard__grid">
-                <?php
-                    include_once __DIR__ .'/templates/admin-sidebar.php';  
-                ?>
-
-                <main class="dashboard__contenido">
-                    <?php 
-                        echo $contenido; 
-                    ?> 
-                </main>
-            </div>
-
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js" integrity="sha512-L0Shl7nXXzIlBSUUPpxrokqq4ojqgZFQczTYlGjzONGTDAcLremjwaWv5A+EDLnxhQzY5xUZPWLOLqYRkY0Cbw==" crossorigin="anonymous" referrerpolicy="no-referrer">
-        </script>
+        <?php 
+            include_once __DIR__ .'/templates/admin-header.php';
+        ?>
         
+        <div class="dashboard__grid">
+            <?php
+                include_once __DIR__ .'/templates/admin-sidebar.php';  
+            ?>
+
+            <main class="dashboard__contenido">
+                <?php 
+                    echo $contenido; 
+                ?> 
+            </main>
+        </div>
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js" integrity="sha512-L0Shl7nXXzIlBSUUPpxrokqq4ojqgZFQczTYlGjzONGTDAcLremjwaWv5A+EDLnxhQzY5xUZPWLOLqYRkY0Cbw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js" integrity="sha512-ElRFoEQdI5Ht6kZvyzXhYG9NqjtkmlkfYk0wr6wHxU9JEHakS7UJZNeml5ALk+8IKlU6jDgMabC3vkumRokgJA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
+
+        <!-- <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> -->
         <script src="/build/js/main.min.js" defer></script>
     </body>
 </html>
